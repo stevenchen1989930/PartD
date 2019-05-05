aab72d5768f19dfced1d3488c53ffe010a651c14
Chen Dong <stevenchen1989930@gmail.com>
2019-05-06 09:35:16 +1200

    Fourth upload of the project.

--- PartD/src/Main.java
+++ PartD/src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 		System.out.println("Hi, my name is Dong Chen");
 		System.out.println("My favourite colour is grey");
 		System.out.println("My favourite mode of transportation is Boats");
+		System.out.println("The best day of the week is Friday");
 	}
 
 }
